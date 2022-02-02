ba36c8431c651f7f43ae8b3e568bfc1b65db7d5a
XiaoboLinlin <xiaobolin11@outlook.com>
2022-02-02 17:40:38 -0500

    new updates

---
+++ firsttool/analysis/analysis.py
@@ -0,0 +1,8 @@
+import mbuild as mb
+import numpy as np
+import parmed as pmd
+from foyer import Forcefield
+import MDAnalysis as mda
+
+
+class an
--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 setup(name='firsttool',
         version='0.1',
         description='Various tools for molecular dynamics',
-        # url='http://github.com/rmatsum836/ramtools',
         author='Xiaobo Lin',
         author_email='xiaobo.lin@vanderbilt.edu',
         license='MIT',
